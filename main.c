0fd8436c33f24cede600f240fb770a6667012cac
andrelrocha <andrerocha0911@gmail.com>
2023-06-09 10:23:03 -0300

    feat: ler mantimentos e adicionar mantimentos

--- main.c
+++ main.c
@@ -21,152 +21,63 @@ typedef struct {
     int quantidade;
 } Mantimento;
 
-typedef struct {
-    Mantimento mantimentos[MAX_MANTIMENTOS];
-    int quantidadeMantimentos;
-    float saldo;
-} Estoque;
+int adicionarMantimento(int saldo, const char *nome, int codigo, float preco, int quantidade) {
 
+    if (saldo - (preco*quantidade) < 0) {
+        printf("Saldo insuficiente para pagar o mantimento!\n");
+        return 0;
+    } else {
+        FILE *arquivo = fopen("estoque.txt", "a");
 
-
-void adicionarMantimento(Estoque *estoque, const char *nome, int codigo, float preco, int quantidade) {
-    if (estoque->quantidadeMantimentos >= MAX_MANTIMENTOS) {
-        printf("Estoque cheio!!!\n");
-        return;
+        fprintf(arquivo, "nome: %s, codigo: %d, preco: %.2f, quantidade: %d ", nome, codigo, preco, quantidade);
+        fprintf(arquivo, "\n"); 
+                    
+        fclose(arquivo);
+        return saldo - ((preco)*quantidade);
     }
-
-    Mantimento novoMantimento;
-    strcpy(novoMantimento.nome, nome);
-    novoMantimento.codigo = codigo;
-    novoMantimento.preco = preco;
-    novoMantimento.quantidade = quantidade;
-
-    estoque->mantimentos[estoque->quantidadeMantimentos++] = novoMantimento;
+        
+    //FALTA ADICIONAR PARTE QUE BUSCA PRODUTOS COM NOME JÁ CADASTRADO E ALTERA A QUANTIDADE
+    //TRANSFORMAR ENTRADA DO SCANF EM MINUSCULO
 }
 
-void removerMantimento(Estoque *estoque, int codigo) {
-    char d[10];
-    
-    printf("Deletar Mantimento");
-    scanf(" %s",d);
+int lerMantimento() {
+    FILE *arquivo = fopen("estoque.txt", "r");
 
-
-    
-    for (int i = 0; i < estoque->quantidadeMantimentos; i++) {
-        if (estoque->mantimentos[i].codigo == codigo) {
-            for (int j = i; j < estoque->quantidadeMantimentos - 1; j++) {
-                estoque->mantimentos[j] = estoque->mantimentos[j + 1];
-            }
-            estoque->quantidadeMantimentos--;
-            printf("Mantimento removido do estoque.\n");
-            return;
-        }
-    }
-    printf("Mantimento com o codigo %d nao encontrado no estoque.\n", codigo);
-}
-
-void editarMantimento(Estoque *estoque, int codigo, float novoPreco, int novaQuantidade) {
-    for (int i = 0; i < estoque->quantidadeMantimentos; i++) {
-        if (estoque->mantimentos[i].codigo == codigo) {
-            estoque->mantimentos[i].preco = novoPreco;
-            estoque->mantimentos[i].quantidade = novaQuantidade;
-            printf("Mantimento editado com sucesso.\n");
-            return;
-        }
-    }
-    printf("Mantimento com o codigo %d nao encontrado no estoque.\n", codigo);
-}
-
-void visualizarEstoque(const Estoque *estoque) {
-    printf("----- Estoque -----\n");
-    int i;
-    for (i = 0; i < estoque->quantidadeMantimentos; i++) {
-        printf("Nome: %s\n", estoque->mantimentos[i].nome);
-        printf("Codigo: %d\n", estoque->mantimentos[i].codigo);
-        printf("Preco: %.2f\n", estoque->mantimentos[i].preco);
-        printf("Quantidade: %d\n", estoque->mantimentos[i].quantidade);
-        printf("-------------------\n");
-    }
-    printf("Saldo disponivel: %.2f\n", estoque->saldo);
-}
-
-void salvarEstoque(const Estoque *estoque) {
-    FILE *arquivo = fopen(ARQUIVO_ESTOQUE, "w");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo do estoque.\n");
-        return;
-    }
-
-    fprintf(arquivo, "%d\n", estoque->quantidadeMantimentos);
-    fprintf(arquivo, "%.2f\n", estoque->saldo);
-
-    int i;
-    for (i = 0; i < estoque->quantidadeMantimentos; i++) {
-        fprintf(arquivo, "%s;%d;%.2f;%d\n", estoque->mantimentos[i].nome, estoque->mantimentos[i].codigo,
-                estoque->mantimentos[i].preco, estoque->mantimentos[i].quantidade);
+    char linha[50];
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        printf("%s\n", linha);
     }
 
     fclose(arquivo);
 }
 
-void carregarEstoque(Estoque *estoque) {
-    FILE *arquivo = fopen(ARQUIVO_ESTOQUE, "r");
-    if (arquivo == NULL) {
-        printf("Arquivo do estoque nao encontrado. Criando novo arquivo.\n");
-        return;
+
+
+
+
+
+
+
+
+
+void main() {
+
+    float saldo = 1000.0;
+    int escolha;
+
+    printf("Qual operacao voce deseja realizar?\n");
+    printf("1-visualizar estoque\n2-adicionar mantimento\n3-comprar mantimento\n4-editar mantimento\n5-remover mantimento\n");
+    scanf(" %d", &escolha);
+
+    switch (escolha)
+    {
+    case 1:
+        lerMantimento();
+        break;
+    case 2: 
+        adicionarMantimento(saldo, "produto", 123454, 3, 1);
+        break;
+    default:
+        break;
     }
-
-    fscanf(arquivo, "%d\n", &estoque->quantidadeMantimentos);
-    fscanf(arquivo, "%f\n", &estoque->saldo);
-
-    int i;
-    for (i = 0; i < estoque->quantidadeMantimentos; i++) {
-        fscanf(arquivo, "%[^;];%d;%f;%d\n", estoque->mantimentos[i].nome, &estoque->mantimentos[i].codigo,
-               &estoque->mantimentos[i].preco, &estoque->mantimentos[i].quantidade);
-    }
-
-    fclose(arquivo);
-}
-
-void comprarMantimento(Estoque *estoque, int codigo, float preco) {
-    for (int i = 0; i < estoque->quantidadeMantimentos; i++) {
-        if (estoque->mantimentos[i].codigo == codigo) {
-            if (estoque->saldo >= preco) {
-                estoque->mantimentos[i].quantidade++;
-                estoque->saldo -= preco;
-                printf("Mantimento comprado com sucesso.\n");
-                salvarEstoque(estoque);
-            } else {
-                printf("Saldo insuficiente para comprar o mantimento.\n");
-            }
-            return;
-        }
-    }
-    printf("Mantimento com o codigo %d nao encontrado no estoque.\n", codigo);
-}
-
-int main() {
-    Estoque estoque;
-    estoque.quantidadeMantimentos = 0;
-    estoque.saldo = 1000.0; // Saldo inicial da lanchonete
-
-    carregarEstoque(&estoque);
-
-    adicionarMantimento(&estoque, "Pão", 1, 1.5, 10);
-    adicionarMantimento(&estoque, "Queijo", 2, 3.0, 5);
-    adicionarMantimento(&estoque, "Presunto", 3, 2.0, 7);
-
-    visualizarEstoque(&estoque);
-
-    comprarMantimento(&estoque, 2, 3.0); // Compra de queijo
-    comprarMantimento(&estoque, 4, 1.5); // Tentativa de compra de mantimento inexistente
-    comprarMantimento(&estoque, 1, 5.0); // Tentativa de compra de pão com saldo insuficiente
-
-    visualizarEstoque(&estoque);
-
-    removerMantimento(&estoque, 3); // Remoção de presunto
-    editarMantimento(&estoque, 2, 4.0, 3); 
-    visualizarEstoque(&estoque);
-
-    salvarEstoque(&estoque); // Salvar o estoque final antes de encerrar o programa
 }
